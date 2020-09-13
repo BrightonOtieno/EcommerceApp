bbfdbc954be84a185d735d09f106fe953a17878d
brighton hoods <47972490+BrightonOtieno@users.noreply.github.com>
2020-09-13 11:27:26 +0300

    Update settings.py

--- EcommerceApp/EcommerceApp/settings.py
+++ EcommerceApp/EcommerceApp/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(4(%q37aq2rfmwm!7+8tc&0d-x2l2c_o#0e9i*oavkngtt3f9j'
-
+SECRET_KEY = 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
